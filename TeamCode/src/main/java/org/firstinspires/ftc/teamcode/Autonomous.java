ade075d1bdb937fb4aee7e3c289130e66e13c6ed
Tiexiao Wang <tie.xiao.wang@gmail.com>
2016-11-08 16:47:46 -0800

    First Autonomous

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by union on 11/8/16.
+ */
+
+public class Autonomous extends OpMode {
+}
